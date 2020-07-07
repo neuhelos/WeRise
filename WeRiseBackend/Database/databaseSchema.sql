9912e230d7f2d0c655e8c860d2412e8137da3d29
Nilber Remon <nilberremon@gmail.com>
2020-07-07 18:07:04 -0400

    Database Directory and Database Schema

---
+++ WeRiseBackend/Database/databaseSchema.sql
@@ -0,0 +1,61 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS createdWorkshops;
+DROP TABLE IF EXISTS registeredWorkshops;
+DROP TABLE IF EXISTS videoChat;
+DROP TABLE IF EXISTS workshopSkills;
+DROP TABLE IF EXISTS usersSkills;
+DROP TABLE IF EXISTS directMessages;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    firstName text NOT NULL,
+    lastName text NOT NULL,
+    password VARCHAR,
+    email VARCHAR UNIQUE,
+    user_pic VARCHAR,
+    bio VARCHAR,
+    instagram VARCHAR,
+    facebook VARCHAR,
+    twitter VARCHAR,
+    linkedin VARCHAR
+);
+
+CREATE TABLE createdWorkshops(
+id SERIAL PRIMARY KEY,
+user_id INT REFERENCES users(id),
+title VARCHAR(280),
+descriptions VARCHAR,
+date DATE,
+startTime TIMESTAMP,
+endTime TIMESTAMP
+);
+
+CREATE TABLE registeredWorkshops(
+id SERIAL PRIMARY KEY,
+user_id INT REFERENCES users(id) ON DELETE SET NULL,
+workshop_id INT REFERENCES createdWorkshops(id) ON DELETE SET NULL,
+);
+
+CREATE TABLE videoChat(
+id SERIAL PRIMARY KEY,
+workshop_id INT REFERENCES createdWorkshops(id) ON DELETE SET NULL,
+url VARCHAR
+);
+CREATE TABLE usersSkills(
+id SERIAL PRIMARY KEY,
+user_id INT REFERENCES users(id) ON DELETE SET NULL,
+skills VARCHAR
+);
+
+CREATE TABLE workshopSkills(
+id SERIAL PRIMARY KEY,
+workshop_id INT REFERENCES createdWorkshops(id) ON DELETE SET NULL,
+skills VARCHAR
+);
+
+CREATE TABLE directMessages(
+id SERIAL PRIMARY KEY,
+senderId INT REFERENCES users(id) ON DELETE SET NULL,
+recieverId INT REFERENCES users(id) ON DELETE SET NULL,
+body VARCHAR,
+);
