eed27ee484d482f6f0a0c3dcd17c6a8faf6307e2
Nilber Remon <nilberremon@gmail.com>
2020-07-18 21:08:20 -0400

    Workshop Feed State Slice Implementation

--- we-rise/src/features/BaseComponents/Footer.js
+++ we-rise/src/features/BaseComponents/Footer.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
 import BottomNavigation from '@material-ui/core/BottomNavigation';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles( (theme) => ({
+    root: {
+        backgroundColor: '#F5F5F5',
+        //background: 'linear-gradient(90deg, hsla(238, 34%, 32%, 1) 0%, hsla(333, 100%, 53%, 1) 50%, hsla(33, 94%, 57%, 1) 100%)',
+        color: 'white',
+    }
+}))
 
 const Footer = () => {
+
+    const classes = useStyles()
+
     return (
-        <BottomNavigation>
-            <a href="https://www.about.com">About</a>
-            <a href='https://www.resources.com'>Resources</a>
+        <BottomNavigation className={classes.root}>
+        
+        
         </BottomNavigation>
     )
 }
--- we-rise/src/features/WorkshopFeed/WorkshopFeed.js
+++ we-rise/src/features/WorkshopFeed/WorkshopFeed.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const WorkshopFeed = () => {
+
+
     return (
         <div>
             WORKSHOP FEED
