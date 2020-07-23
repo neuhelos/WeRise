4d22df642c57d7e7b3cb219a79efa52e0f5f1d86
Nilber Remon <nilberremon@gmail.com>
2020-07-23 12:25:48 -0400

    DateRangePicker MinDate

--- we-rise/src/features/WorkshopFeed/WorkshopFilterBar.js
+++ we-rise/src/features/WorkshopFeed/WorkshopFilterBar.js
@@ -2,15 +2,27 @@ import React, {useState} from 'react'
 
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css'
+import { makeStyles } from '@material-ui/core/styles'
 
 import { DateRange } from 'react-date-range';
 import Grid from '@material-ui/core/Grid'
 import Modal from '../BaseComponents/Modal'
 import Button from '@material-ui/core/Button';
 
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        fontFamily: 'audiowide'
+    },
+    button: {
+        fontFamily: 'audiowide'
+    }
+}))
+
 const WorkshopFilterBar = () => {
     
-    
+    const classes = useStyles()
+
     const [dateRange, setDateRange] = useState([
         {
         startDate: new Date(),
@@ -32,18 +44,19 @@ const WorkshopFilterBar = () => {
         <>
 
         <Grid>
-
-            <Button variant="contained" color="primary"> Select Date Range </Button>
+            <Button className={classes.button} variant="contained" color="primary" onClick={toggleModal}> Select Date Range </Button>
         </Grid>
         
-            <Modal open={open}>
-                <DateRange
-                    editableDateInputs={true}
-                    onChange={handleDateChange}
-                    moveRangeOnFirstSelection={false}
-                    ranges={dateRange}
-                />
-            </Modal>
+        <Modal open={open} toggleModal={toggleModal}>
+            <DateRange
+                editableDateInputs={true}
+                onChange={handleDateChange}
+                moveRangeOnFirstSelection={false}
+                ranges={dateRange}
+                handleCloseModal={toggleModal}
+                minDate={new Date()}
+            />
+        </Modal>
         </>
     )
 }
