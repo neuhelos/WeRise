3b16db2ec54c09b146cff4fa8fd2ecef9f472779
Nilber Remon <nilberremon@gmail.com>
2020-07-27 12:55:21 -0400

    Landing Page Layout & Design

--- we-rise/src/features/BaseComponents/Footer.js
+++ we-rise/src/features/BaseComponents/Footer.js
@@ -6,7 +6,6 @@ import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles( (theme) => ({
     root: {
         backgroundColor: '#F5F5F5',
-        //background: 'linear-gradient(90deg, hsla(238, 34%, 32%, 1) 0%, hsla(333, 100%, 53%, 1) 50%, hsla(33, 94%, 57%, 1) 100%)',
         color: 'white',
     }
 }))
--- we-rise/src/features/Pages/LandingPage.js
+++ we-rise/src/features/Pages/LandingPage.js
@@ -1,16 +1,37 @@
 import React from 'react'
 
 import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles'
 
 import PublicNavBar from '../NavBar/PublicNavBar'
-
+import SloganVideoSource from '../../styling/Assets/Media/SloganAnimation.mp4'
+import VideoChatGif from '../../styling/Assets/Media/VideoChatParty.gif'
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+    },
+    container: {
+        padding: theme.spacing(2),
+        height: '100%',
+    },
+    paper: {
+        backgroundColor: '#282828',
+        padding: theme.spacing(2),
+    },
+    media: {
+        width: '30rem',
+        height: '20rem',
+        borderRadius: '4px',
+    },
+    video: {
+        width: '25rem',
+        height: '25rem',
+        borderRadius: '4px',
+        border: 'solid 10px #282828',
+        padding: theme.spacing(1),
     }
-
 }))
 
 const LandingPage = () => {
@@ -18,9 +39,20 @@ const LandingPage = () => {
     const classes = useStyles()
     
     return (
-        <Grid container className={classes.root}>
-            <Grid item xs={12}>
-                <PublicNavBar />
+        <Grid container className={classes.root} display="flex" direction="column" wrap='nowrap'>
+            <PublicNavBar />
+            <Grid container item className={classes.container} display="flex" direction="row" justify='space-evenly' alignItems='center' wrap='nowrap' sm={12}>
+                <Grid container className={classes.container} item justify='flex-end' alignItems='center' sm={12} md={6}>
+                    <video className={classes.video} loop='loop' autoPlay='autoplay' muted>
+                        <source src={SloganVideoSource} type="video/mp4"/>
+                        Your browser does not support this video.
+                    </video>
+                </Grid>
+                <Grid container className={classes.container} item justify='flex-start' alignItems='center' sm={12} md={6}>
+                    <Paper className={classes.paper}>
+                        <img className={classes.media} src={VideoChatGif} alt="Video Chat"/>
+                    </Paper>
+                </Grid>
             </Grid>
         </Grid>
     )
