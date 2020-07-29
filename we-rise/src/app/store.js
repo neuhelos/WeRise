70ca2e17b540a1b4b55c4c5cfb5ee11c99510e9c
DejaLFlynn <dejaflynn@pursuit.org>
2020-07-28 20:55:37 -0400

    filename change

--- we-rise/src/app/store.js
+++ we-rise/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authenticationReducer from '../features/Authentication/AuthenticationSlice';
-import workshopFeedReducer from '../features/WorkshopFeed/WorkshopFeedSlice'
+import workshopFeedReducer from '../features/WorkshopFeed/workshopFeedSlice'
 
 export default configureStore({
   reducer: {
