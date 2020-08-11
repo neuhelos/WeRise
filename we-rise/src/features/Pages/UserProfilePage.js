71e4b0dd47eb8c05a39c80b58f229abf2adf3fe0
DejaLFlynn <dejaflynn@pursuit.org>
2020-08-11 10:26:28 -0400

    left align and placed elements in UI format instead of raw css

--- we-rise/src/features/Pages/UserProfilePage.js
+++ we-rise/src/features/Pages/UserProfilePage.js
@@ -13,18 +13,29 @@ import CardContent from "@material-ui/core/CardContent";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
 import axios from "axios";
-import '../../styling/UserProfilePage.css'
+import "../../styling/UserProfilePage.css";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    width: "100%"
   },
   paper: {},
+  text: {
+    fontFamily: "audiowide",
+    fontSize: 18,
+    color: 'white'
+   
+  },
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+  },
 }));
 
 const UserProfilePage = () => {
   const classes = useStyles();
   const currentUser = useSelector((state) => state.currentUserSession.uid);
-  
+
   const [profile, setProfile] = useState([]);
   const [firstn, setFirstn] = useState("");
   const [lastn, setLastn] = useState("");
@@ -36,14 +47,14 @@ const UserProfilePage = () => {
     try {
       let res = await axios.get(`${API}/users/${currentUser}`);
       console.log(setProfile(res.data.payload[0]));
-      setProfile(res.data.payload[0])
+      setProfile(res.data.payload[0]);
       setFirstn(res.data.payload[0].firstn);
       setLastn(res.data.payload[0].lastn);
       setEmail(res.data.payload[0].email);
       setBio(res.data.payload[0].bio);
       setPic(res.data.payload[0].user_pic);
-    } catch(error) {
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
   };
   useEffect(() => {
@@ -51,35 +62,44 @@ const UserProfilePage = () => {
   }, []);
 
   return (
-    
-    <div className='userProfile'>
-    <Grid
-      container
-      className={classes.root}
-      container
-      display="flex"
-      direction="column"
-      justify="center"
-      alignItems="center"
-      wrap="nowrap"
-      
+    <div className="userProfile">
+      <Grid
+        container
+        className={classes.root}
+        container
+        display="flex"
+        direction="column"
+        justify="left"
+        alignItems="left"
+        wrap="nowrap"
       >
-    
-      <Card className="Container" />
-      <CardHeader title={firstn, lastn} subheader="User Name" />
-      <CardMedia className={classes.media} image={pic} />
-      <CardContent value={firstn, lastn} image={pic}>
-      <Avatar alt='profilePic' src={pic} className={classes.large}></Avatar>
-        <h2>{firstn}</h2>
-        <h2>{lastn}</h2>
-        <img src={pic}></img>
-        <h3>{email}</h3>
-        <Typography>{bio}</Typography>
-        {/* <h3>{bio}</h3> */}
-      </CardContent>
-    </Grid>
+        <Card className="Container" />
+        <CardHeader
+          className={classes.header}
+          avatar={
+            <Avatar aria-label="user" className={classes.avatar} src={pic} />
+          }
+          title={
+            <Typography className={classes.text}>
+              {" "}
+              {firstn} {lastn} 
+            </Typography>
+          }
+          subheader={
+          <>
+          
+          <Typography className={classes.text}>{email}</Typography> 
+          </>
+        }
+        />
+        <CardMedia className={classes.media} image={pic} />
+        <CardContent value={(firstn, lastn)} image={pic}>
+ 
+          <Typography>{bio}</Typography>
+        </CardContent>
+      </Grid>
       <UserWorkshopAgenda />
-      </div>
+    </div>
   );
 };
 
