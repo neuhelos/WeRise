246a1bbd0a707120878425a4a5d2ad416a635c0a
Nilber Remon <nilberremon@gmail.com>
2020-08-11 21:44:41 -0400

    Mailgun API and Email Routes Set-Up

--- WeRiseBackend/middleware/Mailgun.js
+++ WeRiseBackend/middleware/Mailgun.js
@@ -1,21 +1,38 @@
 const mailgun = require("mailgun-js");
 
-const DOMAIN = 'sandboxda36fb91034a461aa7163092e536e8d6.mailgun.org';
+const domain = 'sandboxda36fb91034a461aa7163092e536e8d6.mailgun.org';
 const mailgunAPI = mailgun({
     apiKey: process.env.MAILGUN_API_KEY, 
-    domain: DOMAIN
+    domain: domain
 });
 
-const sendEmail = (from, to, subject, text) => {
+const email = (from, to, subject, content) => {
     const data = {
         from: from,
         to: to,
         subject: subject,
-        text: body
+        html: content
     };
     return mailgunAPI.messages().send(data, (error, body) => {
         console.log(body)
+    })
+}
+
+const sendEmail = async (req, res, next) => {
+    console.log(req.body)
+    try {
+        await email(req.body.from, req.body.to, req.body.subject, req.body.content)
+        res.status(200).json({
+            status: "Success",
+            message: "Email Sent",
+        });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            status: "Error",
+            message: error
+        })
     }
 }
 
-
+module.exports = { sendEmail }
