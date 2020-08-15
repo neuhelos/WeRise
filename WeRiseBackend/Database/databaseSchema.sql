f484c8c464cd0d1c1658c216fcbc6b210f5453a8
Nilber Remon <nilberremon@gmail.com>
2020-08-15 10:20:15 -0400

    Database Seeding

--- WeRiseBackend/Database/databaseSchema.sql
+++ WeRiseBackend/Database/databaseSchema.sql
@@ -86,8 +86,8 @@ VALUES
 ('3', '2020-06-23 18:00-04', '5', 'Drama Therapy in COVID Times', 'Express Yourself while Socially Distancing', '2020-09-23 18:00-04', '2020-09-23 19:00-04' , 'Film, Photography & Theatre', 3, 'https://www.marinshakespeare.org/wp-content/uploads/2018/10/Ariel-DSC_0749.jpg'),
 ('4', '2020-07-01 17:00-04', '4', 'I Love Javascript', 'Start Your Coding Journey with a Bit of JS Basics', '2020-10-01 17:00-04', '2020-10-01 18:00-04' ,'Technology, Coding & Programming', 2, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRkILRijL6r1-ctV9uojAyUxg21EZLwjWOMAg&usqp=CAU'),
 ('5', '2020-08-14 16:00-04', '3', 'Werq it Vogue', 'Learn how to burn the runway with your vogue moves', '2020-10-16 16:00-04', '2020-10-16 17:00-04', 'Dance & Music', 5,'https://i2.wp.com/nypost.com/wp-content/uploads/sites/2/2019/06/21a.mjheaderta_rr.jpg?quality=80&strip=all&ssl=1'),
-('6', '2020-07-25 18:00-04', '3', 'Accessorizing Your Wardrobe', 'Its never too much, elevating your style with accessories.', '2020-09-16 18:00-04', '2020-09-16 19:00-04', 'Beauty & Fashion', 5, 'image'),
-('7','2020-07-10 15:00-04', '3', 'Creating Community Change Through Tech', 'Brainstorming strategies for sharing technical know-how and skills for driving social justice in our communities', '2020-10-01 17:00-04', '2020-10-01 20:00-04', 'Community Organizing & Activism', 5, 'image');
+('6', '2020-07-25 18:00-04', '3', 'Accessorizing Your Wardrobe', 'Its never too much, elevating your style with accessories.', '2020-09-16 18:00-04', '2020-09-16 19:00-04', 'Beauty & Fashion', 5, 'http://localhost:3000/AccessoriesWorkshop.jpg'),
+('7','2020-07-10 15:00-04', '3', 'Creating Community Change Through Tech', 'Brainstorming strategies for sharing technical know-how and skills for driving social justice in our communities', '2020-10-01 17:00-04', '2020-10-01 20:00-04', 'Community Organizing & Activism', 5, 'http://localhost:3000/CommunityRainbow.jpg');
 
 INSERT INTO registered_workshops
 (user_id, workshop_id)
@@ -101,7 +101,8 @@ VALUES
 ('E093QRw1swaW4KCQUON44IU2gcy2', '2'),
 ('E093QRw1swaW4KCQUON44IU2gcy2', '3'),
 ('E093QRw1swaW4KCQUON44IU2gcy2', '4'),
-('E093QRw1swaW4KCQUON44IU2gcy2', '5');
+('E093QRw1swaW4KCQUON44IU2gcy2', '5'),
+('E093QRw1swaW4KCQUON44IU2gcy2', '7');
 
 INSERT INTO users_skills
 (id, user_id, skills)
