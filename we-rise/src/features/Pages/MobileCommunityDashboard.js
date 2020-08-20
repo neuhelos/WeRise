8a1eb0955c366a7b25cb385bf558889596c386c1
Nilber Remon <nilberremon@gmail.com>
2020-08-20 11:50:14 -0400

    Mobile Community Dashboard Styling

--- we-rise/src/features/Pages/MobileCommunityDashboard.js
+++ we-rise/src/features/Pages/MobileCommunityDashboard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import SwipeableViews from 'react-swipeable-views';
 
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
@@ -18,41 +17,49 @@ import WorkshopFeedSearchForm from '../WorkshopFeed/WorkshopFeedSearchForm'
 import UserWorkshopAgenda from '../UserWorkshopsAgenda/UserWorkshopsAgenda'
 import RecentlyPostedWorkshops from '../RecentlyPostedWorkshops/RecentlyPostedWorkshops'
 
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`full-width-tabpanel-${index}`}
-        aria-labelledby={`full-width-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box p={3}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-  function a11yProps(index) {
-    return {
-      id: `full-width-tab-${index}`,
-      'aria-controls': `full-width-tabpanel-${index}`,
-    };
-  }
-  
+
 const useStyles = makeStyles((theme) => ({
     root: {
         width: '100%',
         '& *': {
             fontFamily: 'audiowide'
-        }
+        },
+        flex: 1
     },
+    tabBar: {
+        backgroundColor: '#F5F5F5'
+    }
 }));
+
+const TabPanel = (props) => {
+    const { children, value, index, ...other } = props;
+
+    const classes = useStyles()
+
+    return (
+        <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`tabpanel-${index}`}
+        aria-labelledby={`tab-${index}`}
+        {...other}
+        >
+        {value === index && (
+            <Box p={3}>
+            <Typography>{children}</Typography>
+            </Box>
+        )}
+        </div>
+    );
+    }
+
+const a11yProps = (index) => {
+    return {
+        id: `tab-${index}`,
+        'aria-controls': `tabpanel-${index}`,
+    };
+}
+
 
 const MobileCommunityDashboard = () => {
 
@@ -61,47 +68,47 @@ const MobileCommunityDashboard = () => {
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
-    setValue(newValue);
+        setValue(newValue);
     };
 
     const handleChangeIndex = (index) => {
-    setValue(index);
+        setValue(index);
     };
-
 
     return (
         <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-        >
-          <Tab label="Workshop Search" {...a11yProps(0)} />
-          <Tab label="Workshops I'm Attending" {...a11yProps(1)} />
-          <Tab label="Recently Posted Workshops" {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-        <TabPanel value={value} index={0} dir={theme.direction}>
-            <WorkshopFeedSearchForm />
-            <WorkshopFeed />
-        </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
-            <UserWorkshopAgenda />
-        </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-            <RecentlyPostedWorkshops />
-        </TabPanel>
-      </SwipeableViews>
-      </div>
+            <AppBar position="static" color="default">
+                <Tabs
+                    className={classes.tabBar}
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant='fullWidth'
+                    aria-label="mobiledashboard-tab-bar"
+                >
+                    <Tab label="Workshop Search" {...a11yProps(0)} />
+                    <Tab label="Workshops I'm Attending" {...a11yProps(1)} />
+                    <Tab label="Recently Posted Workshops" {...a11yProps(2)} />
+                </Tabs>
+            </AppBar>
+            <SwipeableViews
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+            >
+                <TabPanel value={value} index={0} dir={theme.direction}>
+                    <WorkshopFeedSearchForm />
+                    <WorkshopFeed />
+                </TabPanel>
+                <TabPanel value={value} index={1} dir={theme.direction}>
+                    <UserWorkshopAgenda />
+                </TabPanel>
+                <TabPanel value={value} index={2} dir={theme.direction}>
+                    <RecentlyPostedWorkshops />
+                </TabPanel>
+            </SwipeableViews>
+        </div>
     )
 }
 
