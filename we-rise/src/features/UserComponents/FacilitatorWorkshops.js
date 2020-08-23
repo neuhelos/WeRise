1362386d44fc107454a142586d51c832e36d9223
John Jones Jr <jayjohnjones@gmail.com>
2020-08-23 18:00:00 -0400

    workshops Facilitated

--- we-rise/src/features/UserComponents/FacilitatorWorkshops.js
+++ we-rise/src/features/UserComponents/FacilitatorWorkshops.js
@@ -1,19 +1,58 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useDispatch, useSelector } from "react-redux";
+import { apiURL } from '../../Utilities/apiURL';
+import FacilitatorWorkshopCard from './FacilitatorWorkshopCard'
+
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography';
+import '../../styling/facilitatorCss.css'
 
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        fontFamily: 'audiowide',
+        width: '100%'
+    },
+}))
 
-const FacilitatorWorkshops = (user_id) => {
+ 
+
+const FacilitatorWorkshops = () => {
+    const classes = useStyles();
     const currentUser = useSelector( state => state.currentUserSession.uid );
-    if(!user_id){
-        user_id = currentUser
-    }
-    const UserCreatedWorkshops = useSelector( state => state.workshopFeed);
+    let user_id = currentUser;
+    const [UserCreatedWorkshops, setUCWorkshops] = useState([]);
 
-   
-    return(
-        <h1>Hey Now</h1>
+    const getWorkshops = async() => {
+        try{
+           let res = await axios.get(`${apiURL()}/workshops/${user_id}`)
+            setUCWorkshops(res.data.payload); 
+        }
+        catch(err){
+            console.log(err)
+            throw Error(err)
+        }
+    }
+    useEffect(() => {
+        getWorkshops();
+    },[])
+    
+// debugger 
+    let workshops = UserCreatedWorkshops.map(workshop => {
+        return <FacilitatorWorkshopCard key={workshop.workshop_id} id={workshop.workshop_id} workshop={workshop}/>
+    })
+    
+    return (
+        <div className ='FacilitatorWorkshops'>
+        <h1>Workshops I'm Facilitating</h1>
+            <Grid className={classes.root} container display="flex" direction="column" justify="center" alignItems="center" wrap='nowrap'>
+                {workshops}
+            </Grid> 
+        </div>
     )
+    
 
 }
 
