068574e95964211f22c93b7c0625a66162dccb58
Nilber Remon <nilberremon@gmail.com>
2020-09-03 03:18:35 -0400

    Registered Workshop Modal and Unregistration Stepper

--- we-rise/src/features/RegisteredWorkshops/RegisteredWorkshopModal.js
+++ we-rise/src/features/RegisteredWorkshops/RegisteredWorkshopModal.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
+
 import WorkshopDetails from '../WorkshopFeed/WorkshopDetails'
+import Modal from '../BaseComponents/Modal'
 
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
@@ -27,15 +29,57 @@ const RegisteredWorkshopModal = ({ handleCloseModal, workshop, ...props }) => {
     const classes = useStyles();
     const dispatch = useDispatch();
 
-    return (
+    const [activeStep, setActiveStep] = useState(0);
+
+    const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const JoinWorkshopVideoChat = () => {
+
         <Grid className={classes.root} container display="flex" direction="column" justify="center" alignItems="center">
             <WorkshopDetails workshop={workshop} {...props}/>
             <Grid className={classes.root} container display="flex" direction="row" justify="space-evenly" alignItems="center">
                 { new Date() >= new Date(workshop.start_time) - 6.048e+8 ? <Button variant="contained" color="primary" type="submit" onClick = {() => history.push(`/videoConference/${workshop.user_id}${workshop.workshop_id}`)}>Join VideoChat</Button> : null}
                 <Button variant="contained" color="primary" onClick={handleCloseModal}>Close</Button>
-                <Button variant="contained" color="primary" type="submit" onClick = {() => dispatch(deleteRegistration(workshop.id))}>Unregister</Button> 
+                <Button variant="contained" color="primary" type="submit" onClick={handleNext}>Cancel Registration</Button> 
             </Grid>
         </Grid>
+    }
+
+    const UnregisterWorkshop = () => {
+    
+    
+    }
+
+    const getStepContent = (step) => {
+        switch (step) {
+            case 0:
+            return <JoinWorkshopVideoChat />
+            case 1:
+            return <UnregisterWorkshop />
+            default:
+            return 'Unknown step';
+        }
+    }
+
+    return (
+        <>
+
+        <Grid className={classes.roott} container display="flex" direction="column" justify="space-evenly" alignItems="center">{getStepContent(activeStep)}</Grid>
+
+
+        
+            <Grid className={classes.root} container display="flex" direction="row" justify="space-evenly" alignItems="center">
+                <Button variant="contained" color="primary" onClick={handleBack}>Cancel</Button>
+                <Button variant="contained" color="primary" type="submit" onClick = {() => dispatch(deleteRegistration(workshop.id))}>Unregister</Button> 
+            </Grid>
+
+        </>
     )
 }
 
