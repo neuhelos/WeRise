44b17e429ff07139d4d26c9d30a643db2b8fa8c1
Nilber Remon <nilberremon@gmail.com>
2020-09-06 13:43:21 -0400

    Landing Page Redev

--- we-rise/src/features/Pages/LandingPageWorkshopCard.js
+++ we-rise/src/features/Pages/LandingPageWorkshopCard.js
@@ -11,14 +11,30 @@ import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Paper from '@material-ui/core/Paper'
 
 import { dateFormat } from '../../Utilities/dateFormat'
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        maxWidth: 400,
+        maxWidth: '100%',
         },
+    paperWrapper : {
+        width: '25%',
+        backgroundColor: '#282828',
+        padding: theme.spacing(1),
+        '&:hover': {
+            border: '2px solid  #FF0F7B',
+            cursor: 'pointer'
+        },
+        margin: theme.spacing(2)
+    },
+    paper: { 
+        width: '100%',
+        backgroundColor: '#666666',
+        padding: theme.spacing(1),
+    },
     media: {
         height: 0,
         paddingTop: '56.25%', // 16:9
@@ -51,47 +67,55 @@ const LandingPageWorkshopCard = ({workshop}) => {
     let time = dateFormat(workshop.start_time).time
 
     return (
-    <Card className={classes.root}>
-        <CardHeader
-        avatar={
-            <Avatar aria-label="recipe" className={classes.avatar} src={workshop.user_pic} alt={workshop.firstn.toUpperCase()}>
-            </Avatar>
-        }
-        title={workshop.title}
-        subheader={
-            <>
-            <Typography className={classes.text}>{`${date}`}</Typography>
-            <Typography className={classes.text}>{`${time}`}</Typography>
-            </>
-        }
-        />
 
-        <CardMedia
-        className={classes.media}
-        image={workshop.workshop_img}
-        title={workshop.title}
-        />
-        <CardActions disableSpacing>
-        <IconButton
-            className={clsx(classes.expand, {[classes.expandOpen]: expanded,})}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-        >
-            <ExpandMoreIcon />
-        </IconButton>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-                <Typography>
-                    Category: {workshop.category}
-                </Typography>
-                <Typography paragraph>
-                    Description: {workshop.descriptions}
-                </Typography>
-            </CardContent>
-        </Collapse>
-    </Card>
+        <Paper  className={classes.paperWrapper}>
+            <Paper className={classes.paper}>
+                <Card className={classes.root}>
+                    <CardHeader
+                    avatar={
+                        <Avatar aria-label="recipe" className={classes.avatar} src={workshop.user_pic} alt={workshop.firstn.toUpperCase()}>
+                        </Avatar>
+                    }
+                    title={workshop.title}
+                    subheader={
+                        <>
+                        <Typography className={classes.text}>{`${date}`}</Typography>
+                        <Typography className={classes.text}>{`${time}`}</Typography>
+                        </>
+                    }
+                    />
+
+                    <CardMedia
+                    className={classes.media}
+                    image={workshop.workshop_img}
+                    title={workshop.title}
+                    />
+                    <CardActions disableSpacing>
+                    <Typography>
+                        Read More >
+                    </Typography>
+                    <IconButton
+                        className={clsx(classes.expand, {[classes.expandOpen]: expanded,})}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </IconButton>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography gutterBottom={true}>
+                                Category: {workshop.category}
+                            </Typography>
+                            <Typography paragraph>
+                                Description: {workshop.descriptions}
+                            </Typography>
+                        </CardContent>
+                    </Collapse>
+                </Card>
+            </Paper>
+        </Paper>
     );
 }
 
