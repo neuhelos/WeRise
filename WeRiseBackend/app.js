52186468e4effc750b399a54836165e1ccd669c2
DejaLFlynn <dejaflynn@pursuit.org>
2020-09-13 12:12:55 -0400

    port changes

--- WeRiseBackend/app.js
+++ WeRiseBackend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const port = 3001;
+const port = process.env.PORT;
 const app = express();
 app.use(cors());
 
